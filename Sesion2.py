f2fdcb25ac10150dff0f266c91bf12a050052f7c
JoseRopero <josemanuelropero@hotmail.com>
2022-12-01 19:47:50 +0100

    Ejercicios

--- Sesion2.py
+++ Sesion2.py
@@ -1,3 +1,6 @@
+from data.countries import countries
+from data.countries_data import countries_data
+
 # EJERCICIO 6 (Find the Runner-Up Score!)
 
 # Dada la hoja de puntaje de los participantes para el Día del Deporte Universitario,
@@ -17,3 +20,49 @@ print(listado_ordenado[-2])
 
 print("")
 print("=====================================================================================================")
+
+# EJERCICIO 7 (Recorrer los países)
+
+# Recorra los países y extraiga todos los países que contengan la palabra land.
+
+
+for pais in countries:
+    pais.lower()
+    if pais.find('land', 0, len(pais)) >= 0:
+        print(pais)
+
+print("")
+print("=====================================================================================================")
+
+# EJERCICIO 8 (Recorrer listas)
+
+# Dada una lista de frutas: ['plátano', 'naranja', 'mango', 'limón'] invierta el orden usando bucle.
+
+lista_Frutas = ['platano', 'naranja', 'mango', 'limon']
+
+for n in reversed(lista_Frutas):
+    print(n)
+print()
+for n in lista_Frutas[::-1]:
+    print(n)
+
+print()
+print("=====================================================================================================")
+
+# PENDIENTE
+# Vaya a la carpeta de datos y use el archivo countries_data.py.
+# 1. ¿Cuál es el número total de idiomas en los datos?
+# 2. Encuentra los diez idiomas más hablados a partir de los datos
+# 3. Encuentra los 10 países más poblados del mundo
+
+# 1
+lenguajes = list()
+for country in countries_data:
+    for language in country['languages']:
+        lenguajes.append(language)
+#dict(zip(lenguajes, map(lambda x: lenguajes.count(x), lenguajes)))
+#print(lenguajes)
+set_languages = set(lenguajes)
+print(len(set_languages))
+
+# 2
