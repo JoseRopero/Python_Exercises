4eafb1cff697232cb2cb5511fac96b8ce5770b1e
JoseRopero <josemanuelropero@hotmail.com>
2022-12-01 19:56:21 +0100

    Ejercicios

--- Sesion2.py
+++ Sesion2.py
@@ -65,4 +65,27 @@ for country in countries_data:
 set_languages = set(lenguajes)
 print(len(set_languages))
 
-# 2
+# SOLUTIONS
+list_data = countries_data
+total_languages_initial = []
+for i in list_data:
+    total_languages_initial.extend(i["languages"])
+print("Languages = ", len(set(total_languages_initial)))
+counts = {}
+for i in total_languages_initial:
+    counts[i] = counts.get(i, 0) + 1
+
+
+def sort_dict_by_value(d, reverse=False):
+    return dict(sorted(d.items(), key=lambda x: x[1], reverse=reverse))
+
+
+counts = sort_dict_by_value(counts, True)
+for i in list(counts.items())[:10]:
+    print(i)
+populations = {}
+for i in list_data:
+    populations[i["name"]] = i["population"]
+populations = sort_dict_by_value(populations, True)
+for i in list(populations.items())[:10]:
+    print(i)
